c7551d8fbb34c589b63e0273a9d422a607361382
John Lee <john.s.lee@nordstrom.com>
2018-10-20 00:39:59 -0700

    Complete up to LL Stanford Problem #8.

---
+++ java-practice/src/com/jguamie/LinkedList.java
@@ -0,0 +1,119 @@
+package com.jguamie;
+
+// Problems from:
+// http://cslibrary.stanford.edu/105/LinkedListProblems.pdf
+public class LinkedList {
+  // Head node pointer. Will be null for an empty linked list.
+  private Node head;
+
+  public void LinkedList() {
+    head = null;
+  }
+
+  // Stanford Problem #1
+  public int count(int searchFor) {
+    if (head == null) {
+      return 0;
+    }
+    Node iterator = head;
+    int counter = 0;
+    while (iterator != null) {
+      if (iterator.data == searchFor) {
+        counter++;
+      }
+      iterator = iterator.next;
+    }
+    return counter;
+  }
+
+  // Stanford Problem #2
+  public int getNth(int index) {
+    if (head == null) {
+      return 0;
+    }
+    Node iterator = head;
+    for (int i = 0; i < index; i++) {
+      if (iterator.next == null) {
+        return 0;
+      }
+      iterator = iterator.next;
+    }
+    return iterator.data;
+  }
+
+  // Stanford Problem #3
+  public void deleteList() {
+    // Garbage collection will handle the rest
+    head = null;
+  }
+
+  // Stanford Problem #4
+  public void pop() {
+    if (head == null) {
+      return;
+    }
+    Node toDelete = head;
+    head = head.next;
+    toDelete = null;
+  }
+
+  // Stanford Problem #5
+  public void insertNth(int index, int data) {
+    Node iterator = head;
+    for (int i = 0; i < index-1; i++) {
+      if (iterator.next == null) {
+        return;
+      }
+      iterator = iterator.next;
+    }
+    Node toInsert = new Node(data);
+    Node nextToInsert = iterator.next;
+    iterator = toInsert;
+    toInsert.next = nextToInsert;
+  }
+
+  // Stanford Problem #6
+  public void sortedInsert(Node head, Node newNode) {
+    if (head == null || newNode.data <= head.data) {
+      newNode.next = head;
+      head = newNode;
+      return;
+    }
+    Node iterator = head;
+    while (iterator.next != null) {
+      if (newNode.data <= iterator.next.data) {
+        newNode.next = iterator.next;
+        iterator.next = newNode;
+        return;
+      }
+    }
+    iterator.next = newNode;
+    // newNode.next is already initialized to null
+    //newNode.next = null;
+  }
+
+  // Stanford Problem #7
+  public void insertSort() {
+    Node iterator = head;
+    Node newHead = null;
+    while(iterator != null) {
+      Node toSort = iterator;
+      iterator = iterator.next;
+      sortedInsert(newHead, toSort);
+    }
+    head = newHead;
+  }
+
+  // Stanford Problem #8
+  public void Append(Node aRef, Node bRef) {
+    Node aIterator = aRef;
+    while (aIterator.next != null) {
+      aIterator = aIterator.next;
+    }
+    aIterator.next = bRef;
+  }
+
+  // Stanford Problem #9
+  public Node frontBackSplit() {
+  }
+}
--- java-practice/src/com/jguamie/Node.java
+++ java-practice/src/com/jguamie/Node.java
@@ -1,11 +1,15 @@
 package com.jguamie;
 
 public class Node {
+  // Linked List
+  Node next;
+  // Binary Tree
   Node left;
   Node right;
   int data;
 
   Node(int newData) {
+    next = null;
     left = null;
     right = null;
     data = newData;
